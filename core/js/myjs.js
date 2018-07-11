eb32d2027d7066ce5cbce2a937c19aa926a153ef
Tyler Goetz <goetztyler813@gmail.com>
2018-07-11 15:36:07 -0500

    6
    almost ready

--- core/js/myjs.js
+++ core/js/myjs.js
@@ -1,34 +1,29 @@
-var elHeight = document.getElementById("bandName").offsetHeight;
-var elWidth = document.getElementById("bandName").offsetWidth;
-var runOnce = true;
-function changeText(){
-  var el = document.getElementById("bandName");
-  $("#bandName").fadeOut("slow", function(){
-    el.innerHTML = "Band Bio stuff here";
-    el.className="diplay-4";
-    $("#bandName").fadeIn("slow", function(){
-    });
-  });
+var runOnce = true
+
+function changeText() {
+  var el = document.getElementById('bandName')
+  $('#bandName').fadeOut('slow', function() {
+    el.innerHTML = 'Band Bio stuff here'
+    el.className = 'diplay-4'
+    $('#bandName').fadeIn('slow', function() {})
+  })
 }
 
-function restoreText(){
-  var el = document.getElementById("bandName");
-  $("#bandName").fadeOut("slow", function(){
-    el.innerHTML = "NEWCOMER";
-    el.className="display-4";
-    $("#bandName").fadeIn("slow", function(){
-    });
-  });
+function restoreText() {
+  var el = document.getElementById('bandName')
+  $('#bandName').fadeOut('slow', function() {
+    el.innerHTML = 'NEWCOMER'
+    el.className = 'display-4'
+    $('#bandName').fadeIn('slow', function() {})
+  })
 }
 
-$(window).scroll(function(){
-  if(runOnce == true){
+$(window).scroll(function() {
+  if (runOnce === true) {
     $('html, body').animate({
-      scrollTop: $("#row2").offset().top
-    }, 1000);
-    console.log('fired: ' +  runOnce);
-    runOnce = false;
-    return;
+      scrollTop: $('#row2').offset().top
+    }, 850)
+    console.log('fired: ' + runOnce)
+    runOnce = false
   }
-  return;
-});
+})
